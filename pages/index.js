5f632381add1374ddd8aa97bac5b5076314c57a2
Chanice Ashley <chaniceashley@gmail.com>
2021-02-14 20:44:07 +0300

    Created Next js app

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <p className="text-3xl text-blue-400">This is initial Next js app</p>
+      </div>
+    </div>
+  )
+}
