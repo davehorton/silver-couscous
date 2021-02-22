dc5dff5b5e053c742b97473d0c50c6311352d0cd
Chanice Ashley <chaniceashley@gmail.com>
2021-02-22 19:40:03 +0300

    Change pricing contents

---
+++ src/constants/landingPages/pricing/index.js
@@ -0,0 +1,52 @@
+export const accounts = [
+  {
+    title: "A capacity of",
+    description: "5 simultaneous calls 5 simultaneously registered devices",
+  },
+  {
+    title: "A prepaid credit",
+    description:
+      "to get you started making outbound calls and using speech services",
+  },
+];
+
+export const readyToGrow = [
+  {
+    title: "Add Capacity",
+    description:
+      "Purchase capacity for the number of active calls and devices you need to support",
+  },
+  {
+    title: "Add Prepaid Funds",
+    description:
+      "Complete calls through your own carrier at no added cost, or fund a prepaid account to cover your calling costs",
+  },
+  {
+    title: "Purchase Add-Ons",
+    description:
+      "Choose from a selection of optional add-ons to extend the functionality of your account",
+  },
+];
+
+export const addOnsTable = [
+  {
+    addOn: "Purchase phone numbers",
+    pricing: "Coming soon",
+  },
+  {
+    addOn: "Port an existing phone number",
+    pricing: "Coming soon",
+  },
+  {
+    addOn: "Enable Microsoft Teams Direct Routing",
+    pricing: "Coming soon",
+  },
+  {
+    addOn: "Provision a Node-RED app server",
+    pricing: "Coming soon",
+  },
+  {
+    addOn: "Private Session Border Controller with monitoring dashboard",
+    pricing: "Coming soon",
+  },
+];
